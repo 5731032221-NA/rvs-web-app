2c66d5e64293cec43217458f62f4c3ae578996fd
Naphol Ayarangsaritkul <naphoaya@metrosystems.co.th>
2022-01-12 23:15:39 +0700

    edit login page

--- src/components/Utils/errorMessage.js
+++ src/components/Utils/errorMessage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import './errorMessage.css'
-
+import WarningIcon from '@mui/icons-material/Warning';
+import {
+  Grid
+} from "@mui/material";
 export default function ErrorMessageComponent({text=""}) {
-  return  <div className="Rectangle-454"><div className="Path-461"/><h5 className="Invalid-Username-or-password" >{text}</h5></div>
+  return  <div className="Rectangle-454"><div className="redLeft"/><WarningIcon className="icon"/><h5 className="Invalid-Username-or-password" >{text}</h5></div>
 }
