958a9a24c4d7d230a68cd699246fa6783bfe80c8
anongnt <anongnat6952@gmail.com>
2022-03-05 03:51:15 +0700

    up

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,29 +1,24 @@
 import * as React from "react";
 import logo_white from "../assets/images/logo_Revosoft.png";
+import logomin_white from "../assets/images/logomin_white.png";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { FormControl, Grid, MenuItem, Select } from "@mui/material";
+import {
+  AppBar,
+  Drawer,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import SortIcon from "@mui/icons-material/Sort";
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from "@material-ui/core";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import clsx from "clsx";
 
 import Header from "../layouts/Header";
 import ListMenu from "./../middleware/listitems/ListMenu";
@@ -37,38 +32,9 @@ const useStyles = makeStyles((theme) => ({
     border: 0,
     boxShadow: "0 3px 5px 2px #ffffff4a",
     color: "white",
+    zIndex: theme.zIndex.drawer + 1,
   },
-  menuButtonHidden: {
-    display: "none",
-  },
-  // logoRevosoft: {
-  //   display: "none",
-  //   [theme.brealpoints.up("sm")]: {
-  //     display: "block",
-  //   },
-  // },
 }));
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(9)} + 1px)`,
-  },
-});
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -79,54 +45,19 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
-
 export default function Main({ children }) {
   const theme = useTheme();
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const { window } = children;
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
   return (
     <Box sx={{ display: "flex" }}>
       <GlobalStyles
@@ -145,38 +76,39 @@ export default function Main({ children }) {
         }}
       />
       <CssBaseline />
-      <AppBar className={classes.header} position="fixed" open={open}>
+      <AppBar className={classes.header} position="fixed">
         <Toolbar>
-          <img
-            className={classes.logoRevosoft}
-            src={logo_white}
-            className={clsx(
-              classes.logoExpand,
-              open && classes.menuButtonHidden
-            )}
-            alt="..."
-            height={40}
-          />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <img src={logo_white} alt="..." height={40} />
+          </Box>
+
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <img src={logomin_white} alt="..." height={40} />
+          </Box>
 
           <IconButton
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            color="inherit"
+            size="large"
             edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerToggle}
             sx={{
-              marginLeft: "20px",
-              marginRight: "36px",
-              ...(open && { display: "none" }),
+              marginLeft: "5px",
             }}
           >
             <SortIcon />
           </IconButton>
-          <div className={classes.sectionDesktop}>
+          <span
+            style={{
+              color: "white",
+              borderLeft: " 1px solid rgb(255 255 255 / 44%)",
+              marginTop: 20,
+              marginBottom: 20,
+            }}
+          ></span>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <span
-              className={clsx(
-                classes.logoExpand,
-                open && classes.menuButtonHidden
-              )}
               style={{
                 color: "white",
                 borderLeft: " 1px solid rgb(255 255 255 / 44%)",
@@ -184,89 +116,150 @@ export default function Main({ children }) {
                 marginBottom: 20,
               }}
             ></span>
-          </div>
+          </Box>
+
           <Header />
         </Toolbar>
       </AppBar>
 
-      <Drawer
-        variant="permanent"
-        open={open}
-        PaperProps={{
-          sx: {
-            backgroundColor: "#1e42a4",
-            color: "white",
-          },
-        }}
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
       >
-        <DrawerHeader sx={{ backgroundColor: "#0f349a", color: "white" }}>
-          <img
-            src={logo_white}
-            className="rounded mx-auto d-block"
-            alt="..."
-            height={40}
-          />
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <SortIcon style={{ color: "white" }} />
-            ) : (
-              <SortIcon style={{ color: "white" }} />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-
-        <Grid
-          item
-          container
-          spacing={1}
-          style={{ paddingLeft: 30, marginTop: 25, paddingBottom: 10 }}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "flex", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "#1e42a4",
+              color: "white",
+            },
+          }}
         >
-          <Grid item container spacing={1}>
-            <Typography
-              variant="subtitle1"
-              style={{ fontSize: 12, paddingLeft: 50, marginTop: -10 }}
-            >
-              {" Change Property"}
-            </Typography>
-          </Grid>
-
-          <Grid sx={{ bgcolor: "#ffff", borderRadius: "5px" }}>
-            <SwapHorizIcon
-              style={{
-                paddingRight: 0,
-                color: "#000",
-                fontSize: 25,
-                marginTop: "10px",
-                marginLeft: "5px",
-              }}
-            />
-            <FormControl
-              variant="filled"
-              style={{ backgroundColor: "white", borderRadius: 5 }}
-            >
-              <Select
-                name="selectprop"
-                id="selectprop"
-                value="Metro Pattaya"
-                style={{ width: 160, height: 40, backgroundColor: "white" }}
+          <Grid
+            item
+            container
+            spacing={1}
+            style={{ paddingLeft: 30, marginTop: 25, paddingBottom: 10 }}
+          >
+            <Grid item container spacing={1}>
+              <Typography
+                variant="subtitle1"
+                style={{ fontSize: 12, paddingLeft: 50, marginTop: -10 }}
               >
-                <MenuItem value="Metro Pattaya" label="Metro Pattaya">
-                  <div style={{ marginTop: -7 }}>Metro Pattaya</div>
-                </MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
+                {" Change Property"}
+              </Typography>
+            </Grid>
 
-        <ListMenu />
-      </Drawer>
+            <Grid sx={{ bgcolor: "#ffff", borderRadius: "5px" }}>
+              <SwapHorizIcon
+                style={{
+                  paddingRight: 0,
+                  color: "#000",
+                  fontSize: 25,
+                  marginTop: "10px",
+                  marginLeft: "5px",
+                }}
+              />
+              <FormControl
+                variant="filled"
+                style={{ backgroundColor: "white", borderRadius: 5 }}
+              >
+                <Select
+                  name="selectprop"
+                  id="selectprop"
+                  value="Metro Pattaya"
+                  style={{ width: 160, height: 40, backgroundColor: "white" }}
+                >
+                  <MenuItem value="Metro Pattaya" label="Metro Pattaya">
+                    <div style={{ marginTop: -7 }}>Metro Pattaya</div>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <ListMenu />
+        </Drawer>
+
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "flex" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "#1e42a4",
+              color: "white",
+            },
+          }}
+          open
+        >
+          <Grid
+            item
+            container
+            spacing={1}
+            style={{ paddingLeft: 30, marginTop: 25, paddingBottom: 10 }}
+          >
+            <Grid item container spacing={1}>
+              <Typography
+                variant="subtitle1"
+                style={{ fontSize: 12, paddingLeft: 50, marginTop: -10 }}
+              >
+                {" Change Property"}
+              </Typography>
+            </Grid>
+
+            <Grid sx={{ bgcolor: "#ffff", borderRadius: "5px" }}>
+              <SwapHorizIcon
+                style={{
+                  paddingRight: 0,
+                  color: "#000",
+                  fontSize: 25,
+                  marginTop: "10px",
+                  marginLeft: "5px",
+                }}
+              />
+              <FormControl
+                variant="filled"
+                style={{ backgroundColor: "white", borderRadius: 5 }}
+              >
+                <Select
+                  name="selectprop"
+                  id="selectprop"
+                  value="Metro Pattaya"
+                  style={{ width: 160, height: 40, backgroundColor: "white" }}
+                >
+                  <MenuItem value="Metro Pattaya" label="Metro Pattaya">
+                    <div style={{ marginTop: -7 }}>Metro Pattaya</div>
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <ListMenu />
+        </Drawer>
+      </Box>
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
 
         {children}
       </Box>
-      <BottomBar />
+
+      <Box sx={{ display: { xs: "none", md: "flex" } }}>
+        <BottomBar />
+      </Box>
     </Box>
   );
 }
